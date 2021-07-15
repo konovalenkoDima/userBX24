90e38dd592e274574efc44056e758608f40e5fd2
konovalenko <dimon200010@gmail.com>
2021-07-15 08:53:34 +0300

    SDK + DB

--- install.php
+++ install.php
@@ -1,15 +1,6 @@
-<?php
-    $json = json_encode($_REQUEST);
-    $file = fopen('credentials.json', 'w+');
-    fwrite($file, $json);
-    fclose($file);
-
-
-    ?>
 <head>
     <script src="//api.bitrix24.com/api/v1/"></script>
     <script>
-        console.log('test');
         BX24.init(function (){
             BX24.installFinish();
         })
